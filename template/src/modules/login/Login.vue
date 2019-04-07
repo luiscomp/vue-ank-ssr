b9795138185ccff2a0623453945b63a0f1bfb3b7
Luis Eduardo <luizeduardo354@gmail.com>
2019-04-07 15:15:13 -0300

    Correção de caracteres de escape

--- template/src/modules/login/Login.vue
+++ template/src/modules/login/Login.vue
@@ -5,16 +5,16 @@
         </div>
         <div class="login-form" v-bind:class="{ 'login-form__visivel': loginVisivel}">
             <div class="login-form--formulario">
-                <h1>{{ $t('login') }}</h1>
+                <h1>\{{ $t('login') }}</h1>
                 <vs-input icon="email" :label-placeholder="$t('email')" />
                 <vs-input icon="lock" :label-placeholder="$t('senha')" style="margin: 20px 0px 10px 0px"/>
                 <div class="formulario-footer">
-                    <vs-button size="small" style="float: right" @click="entrar">{{ $t('entrar') }}</vs-button>
-                    <span style="float: left; font-size: 14px; line-height: 26px; height: 26px">{{ $t('esqueceu_sua_senha') }}</span>
+                    <vs-button size="small" style="float: right" @click="entrar">\{{ $t('entrar') }}</vs-button>
+                    <span style="float: left; font-size: 14px; line-height: 26px; height: 26px">\{{ $t('esqueceu_sua_senha') }}</span>
                 </div>
             </div>
             <div class="login-form--footer">
-                <span>{{ $t('corporacao') }}</span>
+                <span>\{{ $t('corporacao') }}</span>
             </div>
         </div>
     </div>
--- template/src/modules/principal/Principal.vue
+++ template/src/modules/principal/Principal.vue
@@ -3,11 +3,11 @@
         <vs-navbar class="ank-navbar" style="background-color: #FFFFFF">
             <div slot="title">
                 <vs-navbar-title>
-                    {{ $t('app_name') }}
+                    \{{ $t('app_name') }}
                 </vs-navbar-title>
             </div>
             <vs-navbar-item index="0">
-                <a href="/">{{ $t('sair') }}</a>
+                <a href="/">\{{ $t('sair') }}</a>
 
             </vs-navbar-item>
         </vs-navbar>
@@ -27,19 +27,19 @@
                         <template v-for="(submenu, j) in menu.submenus">
                             <vs-sidebar-group v-if="submenu.submenus" :title="$t(submenu.nome)" :key="j">
                                 <vs-sidebar-item v-for="(sub_submenu, k) in submenu.submenus" :index="sub_submenu.nome" :icon="sub_submenu.icone" :key="k" :to="sub_submenu.path">
-                                    {{ $t(sub_submenu.nome) }}
+                                    \{{ $t(sub_submenu.nome) }}
                                 </vs-sidebar-item>
                             </vs-sidebar-group>
 
                             <vs-sidebar-item v-else :index="submenu.nome" :icon="submenu.icone" :key="j" :to="submenu.path">
-                                {{ $t(submenu.nome) }}
+                                \{{ $t(submenu.nome) }}
                             </vs-sidebar-item>
                         </template>
 
                     </vs-sidebar-group>
 
                     <vs-sidebar-item v-else :index="menu.nome" :icon="menu.icone" :key="i" :to="menu.path">
-                        {{ $t(menu.nome) }}
+                        \{{ $t(menu.nome) }}
                     </vs-sidebar-item>
                 </template>
 
