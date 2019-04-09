7cb55bac0e3eac9dd53ee364a3a791cfe9d792a3
Luis Eduardo <luizeduardo354@gmail.com>
2019-04-08 22:51:40 -0300

    Ajustes de estilos

--- template/src/modules/login/LoginView.vue
+++ template/src/modules/login/LoginView.vue
@@ -3,14 +3,14 @@
         <div class="login-bg">
 
         </div>
-        <div class="login" v-bind:class="{ 'login-form__visible': loginVisible}">
+        <div class="login" v-bind:class="{ 'login__visible': loginVisible}">
             <div class="form">
                 <h1>\{{ $t('login') }}</h1>
                 <vs-input icon="email" :label-placeholder="$t('email')" />
                 <vs-input icon="lock" :label-placeholder="$t('password')" style="margin: 20px 0px 10px 0px"/>
                 <div class="form--footer">
-                    <vs-button size="small" @click="signIn">\{{ $t('signin') }}</vs-button>
                     <span>\{{ $t('forgot_password') }}</span>
+                    <vs-button size="small" @click="signIn">\{{ $t('signin') }}</vs-button>
                 </div>
             </div>
             <div class="login--footer">
